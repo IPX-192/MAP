cf33ebb1449bfadb0bb51e9761f68cd70a7b81f1
tianchuyue <894890347@qq.com>
2023-02-14 13:50:25 +0800

    上传model类

---
+++ datamodel/CTagInfoModel.cpp
@@ -0,0 +1,103 @@
+﻿#include "CTagInfoModel.h"
+
+CTagInfoModel::CTagInfoModel(QObject *parent)
+{
+    m_roleName.insert(TagIDRole, "tagID");
+    m_roleName.insert(MapIDRole, "mapID");
+    m_roleName.insert(BatteryRole, "battery");
+    m_roleName.insert(PosXRole, "posX");
+    m_roleName.insert(PosYRole, "posY");
+    m_roleName.insert(PosZRole, "posZ");
+    m_roleName.insert(StaticTimeRole, "staticTime");
+}
+
+void CTagInfoModel::loadData(const vector<CTagInfo> &vecTag)
+{
+    m_TagInfoData.clear();
+
+    for(auto it : vecTag)
+    {
+        m_TagInfoData.push_front(it);
+    }
+
+    emit layoutChanged();
+}
+
+void CTagInfoModel::addData(CTagInfo &info)
+{
+    m_TagInfoData.push_front(info);
+
+    emit layoutChanged();
+}
+
+void CTagInfoModel::addData(vector<CTagInfo> &vecTag)
+{
+    for(auto it : vecTag)
+    {
+        m_TagInfoData.push_back(it);
+    }
+    emit layoutChanged();
+}
+
+void CTagInfoModel::deleteRow(int &rowIndex)
+{
+    if( rowIndex>=0 && rowIndex<m_TagInfoData.size() )
+    {
+        m_TagInfoData.removeAt(rowIndex);
+    }
+    emit layoutChanged();
+}
+
+int CTagInfoModel::rowCount(const QModelIndex &parent) const
+{
+    return m_TagInfoData.size();
+}
+
+QVariant CTagInfoModel::data(const QModelIndex &index, int role) const
+{
+    if (!index.isValid() || index.row() > m_TagInfoData.size())
+    {
+        return QVariant();
+    }
+
+    CTagInfo eleData = m_TagInfoData.at( index.row() );
+
+    switch (role)
+    {
+    case TagIDRole:
+    {
+        return eleData.iTagID();
+    }
+    case MapIDRole:
+    {
+        return eleData.iMapID();
+    }
+    case BatteryRole:
+    {
+        return eleData.iBattery();
+    }
+    case PosXRole:
+    {
+        return eleData.iPosX();
+    }
+    case PosYRole:
+    {
+        return eleData.iPosY();
+    }
+    case PosZRole:
+    {
+        return eleData.iPosZ();
+    }
+    case StaticTimeRole:
+    {
+        return eleData.iStaticTime();
+    }
+    default:
+        return QVariant();
+    }
+}
+
+QHash<int, QByteArray> CTagInfoModel::roleNames() const
+{
+    return m_roleName;
+}
--- datamodel/CUserInfoModel.h
+++ datamodel/CUserInfoModel.h
@@ -1,4 +1,40 @@
 ﻿#ifndef CUSERINFOMODEL_H
 #define CUSERINFOMODEL_H
 
+#include <QAbstractListModel>
+#include "../datamanager/CUserInfo.h"
+
+class CUserInfoModel : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    enum MODEL_ROLE
+    {
+        IsChooseRole = 0,
+        UserNameRole,
+        UserIDRole,
+        UserDepartmentRole,
+        UserRole,
+        UserTagID
+    };
+
+public:
+    explicit CUserInfoModel(QObject *parent = nullptr);
+
+    void loadData(const vector<CUserInfo> &vecUser);
+    void addData(CUserInfo &info);
+    void addData(vector<CUserInfo> &vecUser);
+    void deleteRow(int &rowIndex);
+
+public:
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+private:
+    QVector< CUserInfo > m_userInfoData;
+    QHash<int, QByteArray> m_roleName;
+};
+
 #endif // CUSERINFOMODEL_H
